68e6570fb1f6af8171822b7bd00248e5f3350d7d
Emre Kul <emre.kul@trendyol.com>
2019-09-19 20:24:32 +0300

    backup

--- grpc-example/src/consumer/index.js
+++ grpc-example/src/consumer/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const grpc = require('grpc')
+const grpc = require('grpc');
 const PROTO_PATH = path.join(__dirname, '../introduction.proto');
-const ip = grpc.load(PROTO_PATH).IntroductionService
+const ip = grpc.load(PROTO_PATH).IntroductionService;
 const client = new ip('localhost:50051',grpc.credentials.createInsecure());
 
 client.sayMyName({ name : "emre" }, (err, resp) => {
@@ -9,7 +9,7 @@ client.sayMyName({ name : "emre" }, (err, resp) => {
         console.log(err);
     } else {
         console.log(resp);
-    }    
+    }
 });
 
 
@@ -18,5 +18,5 @@ client.sayMyName({ name : "heisenberg" } , (err, resp) => {
         console.log(err);
     } else {
         console.log(resp);
-    }    
+    }
 });
